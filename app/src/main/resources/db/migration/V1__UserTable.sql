f626b9740fac0b8bc9f11c4d35bf115c5d05e5c0
DavidTrajkovski <davidtrajkovski11@gmail.com>
2021-09-24 20:53:35 +0200

    v1-error za Administrator so dodavanje za uuid

--- app/src/main/resources/db/migration/V1__UserTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE table person(
-    id INTEGER NOT NULL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    pass VARCHAR(100) NOT NULL,
-    mobile VARCHAR(100) NOT NULL
-);
