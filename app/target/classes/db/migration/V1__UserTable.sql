56b9e84c4501dbcca1aa0d50238b40f65bb5ae5e
DavidTrajkovski <davidtrajkovski11@gmail.com>
2021-09-24 20:51:44 +0200

    v1-error za Administrator

---
+++ app/target/classes/db/migration/V1__UserTable.sql
@@ -0,0 +1,8 @@
+CREATE table person(
+    id INTEGER NOT NULL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    pass VARCHAR(100) NOT NULL,
+    mobile VARCHAR(100) NOT NULL
+);
